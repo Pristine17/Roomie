cd5e08a779db34d8b2fc371dc2f971341be13634
satwik mishra <satwik mishra>
2017-03-26 00:29:46 +0530

    First

---
+++ app/src/main/java/com/example/satwik/roomie/tab2.java
@@ -0,0 +1,20 @@
+package com.example.satwik.roomie;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by satwik on 26-03-2017.
+ */
+public class tab2 extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab2, container, false);
+
+        return rootView;
+    }
+}
